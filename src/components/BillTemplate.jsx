0f03c9485c41791775ec266d8de8b77c51145dfc
kanyabhoir <kanyabhoir2209@gmail.com>
2024-08-09 23:27:02 +0530

    date format change and remove some newspepar names and main project title

--- src/components/BillTemplate.jsx
+++ src/components/BillTemplate.jsx
@@ -1,304 +1,9 @@
-// import "./BillTemplate.css";
-// import { useState } from "react";
-// import { WhatsappShareButton, WhatsappIcon } from "react-share";
-// import GenerateImage from "./GenerateImage";
-// import Select from "react-select";
-
-// const newspaperOptions = [
-//   "नवभारत",
-//   "संदयानंद",
-//   "G.M.D.",
-//   "E.M.D.",
-//   "सामना",
-//   "आनंद",
-//   "यंती",
-//   "मनोरमा",
-//   "नवाकाल",
-//   "पुढारी",
-//   "मातृभूमि",
-//   "The Hindu",
-//   "साक्षी",
-//   "A.B.P.",
-//   "B.Line",
-//   "B.Std",
-//   "दिनकरन",
-//   "दिव्या भास्कर",
-//   "क. पत्रिका",
-//   "दिनमान",
-//   "मु. समाचार",
-//   "तरुण भारत",
-//   "नवशक्ति",
-//   "जन्मभूमी",
-//   "उदयवाणी",
-//   "संध्याकाळ",
-//   "दैनिक भास्कर",
-//   "Mint",
-//   "लोकमत",
-//   "H.T.",
-//   "सकाळ",
-//   "F. Press",
-//   "लोकमत",
-//   "H.T.",
-//   "सकाळ",
-//   "F. Press",
-// ].map((name) => ({ label: name, value: name }));
-
-// const BillTemplate = () => {
-//   const [showSummary, setShowSummary] = useState(false);
-//   const [selectedNewspapers, setSelectedNewspapers] = useState([]);
-
-//   const handleNewspaperChange = (selectedOptions) => {
-//     setSelectedNewspapers(selectedOptions || []);
-//   };
-//   const [billData, setBillData] = useState({
-//     name: "",
-//     month: "",
-//     date: "",
-//     newspaper: "",
-//     amount: 0,
-//     googlePay: "",
-//     phonePay: "",
-//     balance: 0,
-//     newspapers: [],
-//   });
-
-//   const handleInputChange = (e) => {
-//     const { name, value } = e.target;
-//     setBillData({
-//       ...billData,
-//       [name]:
-//         name === "amount" || name === "balance" ? parseFloat(value) : value,
-//     });
-//   };
-
-//   const calculateTotal = () => {
-//     // const itemTotal = billData.items.reduce(
-//     //   (sum, item) => sum + item.quantity * item.price,
-//     //   0
-//     // );
-//     // return billData.amount + billData.balance + itemTotal * 0.1;
-
-//     const amount = parseFloat(billData.amount) || 0;
-//     const balance = parseFloat(billData.balance) || 0;
-//     return amount + balance;
-//   };
-
-//   const handleSubmit = () => {
-//     setShowSummary(true);
-//   };
-
-//   const shareText = `
-//   Bill Summary:
-//   Name: ${billData.name}
-//   Month: ${billData.month}
-//   Date: ${billData.date}
-//   Newspaper: ${billData.newspaper}
-//   Amount: ${billData.amount}
-//   Google Pay: ${billData.googlePay}
-//   Phone Pay: ${billData.phonePay}
-//   Balance: ${billData.balance}
-//   Total Amount: ${calculateTotal().toFixed(2)}
-// `;
-
-//   const shareUrl = window.location.href;
-
-//   return (
-//     <div className="main-container">
-//       <h1>Vijay NewsPepar Agency</h1>
-//       <form>
-//         <div className="row">
-//           <div className="column">
-//             <label>Name: </label>
-//             <input
-//               type="text"
-//               name="name"
-//               value={billData.name}
-//               onChange={handleInputChange}
-//             />
-//           </div>
-//           <div className="column">
-//             <label>Month: </label>
-//             <select
-//               name="month"
-//               value={billData.month}
-//               onChange={handleInputChange}
-//             >
-//               <option value="">Select Month</option>
-//               {[
-//                 "January",
-//                 "February",
-//                 "March",
-//                 "April",
-//                 "May",
-//                 "June",
-//                 "July",
-//                 "August",
-//                 "September",
-//                 "October",
-//                 "November",
-//                 "December",
-//               ].map((month) => (
-//                 <option key={month} value={month}>
-//                   {month}
-//                 </option>
-//               ))}
-//             </select>
-//           </div>
-//         </div>
-//         <div className="row">
-//           <div className="column">
-//             <label>Date: </label>
-//             <input
-//               type="date"
-//               name="date"
-//               value={billData.date}
-//               onChange={handleInputChange}
-//             />
-//           </div>
-//           <div className="column">
-//             <label>Newspaper: </label>
-//             <Select
-//               isMulti
-//               options={newspaperOptions}
-//               value={selectedNewspapers}
-//               onChange={handleNewspaperChange}
-//               placeholder="Select Newspapers"
-//             />
-//             {/* <select
-//               name="newspaper"
-//               value={billData.newspaper}
-//               onChange={handleInputChange}
-//             >
-//               <option value="">Select Newspaper</option>
-//               {[
-//                 "नवभारत",
-//                 "संदयानंद",
-//                 "G.M.D.",
-//                 "E.M.D.",
-//                 "सामना",
-//                 "आनंद",
-//                 "यंती",
-//                 "मनोरमा",
-//                 "नवाकाल",
-//                 "पुढारी",
-//                 "मातृभूमि",
-//                 "The Hindu",
-//                 "साक्षी",
-//                 "A.B.P.",
-//                 "B.Line",
-//                 "B.Std",
-//                 "दिनकरन",
-//                 "दिव्या भास्कर",
-//                 "क. पत्रिका",
-//                 "दिनमान",
-//                 "मु. समाचार",
-//                 "तरुण भारत",
-//                 "नवशक्ति",
-//                 "जन्मभूमी",
-//                 "उदयवाणी",
-//                 "संध्याकाळ",
-//                 "दैनिक भास्कर",
-//                 "Mint",
-//                 "लोकमत",
-//                 "H.T.",
-//                 "सकाळ",
-//                 "F. Press",
-//                 "लोकमत",
-//                 "H.T.",
-//                 "सकाळ",
-//                 "F. Press",
-//               ].map((newspaper) => (
-//                 <option key={newspaper} value={newspaper}>
-//                   {newspaper}
-//                 </option>
-//               ))}
-//             </select> */}
-//           </div>
-//         </div>
-//         <div className="row">
-//           <div className="column">
-//             <label> Month Amount: </label>
-//             <input
-//               type="number"
-//               name="amount"
-//               value={billData.amount}
-//               onChange={handleInputChange}
-//             />
-//           </div>
-//           <div className="column">
-//             <label>Google Pay: </label>
-//             <span  style={{
-//                     fontSize: 13,
-//                     marginRight: 30,
-//                     fontWeight: "bold",
-//                     fontFamily: "sans-serif",
-//                     color: "#000",
-//                     border:"1px solid #ccc",
-//                     borderRadius:5,
-//                     padding:7
-
-//                   }}>8793274753</span>
-//           </div>
-//         </div>
-//         <div className="row">
-//           {/* <div className="column">
-//             <label>Phone Pay: </label>
-//             <input
-//               type="text"
-//               name="phonePay"
-//               value={billData.phonePay}
-//               onChange={handleInputChange}
-//             />
-//           </div> */}
-//           <div className="column">
-//             <label>Pre month Balance: </label>
-//             <input
-//               type="number"
-//               name="balance"
-//               value={billData.balance}
-//               onChange={handleInputChange}
-//             />
-//           </div>
-//         </div>
-
-//         <button type="button" onClick={handleSubmit}>
-//           Submit
-//         </button>
-//       </form>
-
-//       {showSummary && (
-//         <>
-//           <div className="share-buttons">
-//             <WhatsappShareButton url={shareUrl} title={shareText}>
-//               <WhatsappIcon size={32} round />
-//             </WhatsappShareButton>
-//           </div>
-//           <div>
-//             <GenerateImage
-//               name={billData.name}
-//               month={billData.month}
-//               date={billData.date}
-//               newspaper={billData.newspaper}
-//               selectedNewspapers={selectedNewspapers}
-//               amount={billData.amount}
-//               googlePay={billData.googlePay}
-//               balance={billData.balance}
-//               total={calculateTotal().toFixed(2)}
-//             />
-//           </div>
-//         </>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default BillTemplate;
-
 import "./BillTemplate.css";
 import { useState } from "react";
 import { WhatsappShareButton, WhatsappIcon } from "react-share";
 import GenerateImage from "./GenerateImage";
 import Select from "react-select";
+import { format } from "date-fns";
 
 const newspaperOptions = [
   "नवभारत",
@@ -309,7 +14,6 @@ const newspaperOptions = [
   "आनंद",
   "यंती",
   "मनोरमा",
-  "नवाकाल",
   "पुढारी",
   "मातृभूमि",
   "The Hindu",
@@ -319,16 +23,11 @@ const newspaperOptions = [
   "B.Std",
   "दिनकरन",
   "दिव्या भास्कर",
-  "क. पत्रिका",
-  "दिनमान",
   "मु. समाचार",
   "तरुण भारत",
-  "नवशक्ति",
   "जन्मभूमी",
   "उदयवाणी",
-  "संध्याकाळ",
   "दैनिक भास्कर",
-  "Mint",
   "लोकमत",
   "H.T.",
   "सकाळ",
@@ -349,7 +48,7 @@ const BillTemplate = () => {
     name: "",
     month: "",
     date: "",
-    googlePay: "",
+    googlePay: "8793274753",
     phonePay: "",
     balance: 0,
   });
@@ -363,13 +62,13 @@ const BillTemplate = () => {
     });
   };
 
+  const formattedDate = billData.date
+  ? format(new Date(billData.date), "dd/MM/yyyy")
+  : "";
+
   const handleAmountChange = (e) => {
     setAmount(e.target.value);
   };
-
-  // const addNewspaperToList = () => {
-
-  // };
 
   const calculateTotal = () => {
     const balance = parseFloat(billData.balance) || 0;
@@ -396,7 +95,7 @@ const BillTemplate = () => {
   Bill Summary:
   Name: ${billData.name}
   Month: ${billData.month}
-  Date: ${billData.date}
+  Date: ${formattedDate}
   Newspapers: ${newspaperList
     .map((item) => item.newspapers.map((n) => n.label).join(", "))
     .join("; ")}
@@ -497,12 +196,8 @@ const BillTemplate = () => {
             <div className="row">
               <div className="column">
                 <label>Google Pay: </label>
-                <input
-                  type="text"
-                  name="googlePay"
-                  value={billData.googlePay}
-                  onChange={handleInputChange}
-                />
+               
+                <span style={{border:"1px solid #ccc",padding:5,borderRadius:5,background:"#fff"}}>8793274753</span>
               </div>
             </div>
 
@@ -525,7 +220,7 @@ const BillTemplate = () => {
           <GenerateImage
             name={billData.name}
             month={billData.month}
-            date={billData.date}
+            date={formattedDate}
             newspaper={billData.newspaper}
             selectedNewspapers={newspaperList}
             googlePay={billData.googlePay}
